0e244af97f7054d0c09c0538b833547019eefd42
Antoine Angot <antoine.angot@gmail.com>
2013-02-09 12:25:56 +0100

    haruhichan v1

---
+++ trunk/haruhichan/sql.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "213.251.169.168";
+$user = "gazoo";
+$db = "gazoo";
+$pass = "";
+echo "PLEASE ADD DB PASSWORD then comment this line\n"; fread(STDIN, 1);
+
+
+
+$link = mysql_connect($host, $user, $pass);
+if (!$link) {
+  die('Could not connect: ' . mysql_error());
+ }
+else {
+    echo "DB: Connected\n";
+}
+
+mysql_select_db($db);
+
+?>
